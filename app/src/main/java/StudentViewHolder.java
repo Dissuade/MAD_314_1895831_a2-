fd6f09f37d6980cb96630835ce940d916445ba8f
dissuade <montrealgurpreet19@gmail.com>
2019-10-19 12:42:11 -0400

    final commit1

--- app/src/main/java/StudentViewHolder.java
+++ app/src/main/java/StudentViewHolder.java
@@ -1,2 +1,20 @@
-public class StudentViewHolder {
+import com.example.mad314a2.R;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+
+public class StudentViewHolder extends ViewHolder {
+
+    public TextView tvName;
+
+    public StudentViewHolder(View itemView) {
+        super(itemView);
+        tvName = itemView.findViewById(R.id.tvName);
+
+    }
+
 }
